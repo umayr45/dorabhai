6afa0ccaa88f2a5d51b22bafd4e5703aa0122449
Umayr Farooqui <umayrfarooqui45@gmail.com>
2020-09-03 19:08:38 +0530

    done

--- src/About.js
+++ src/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './About.css'
 import { Card, CardHeader, CardBody } from 'reactstrap';
+import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
 
 function About() {
     return (
@@ -15,6 +18,84 @@ function About() {
 
                 </CardBody>
             </Card>
+            
+            <div className='class_detail'>
+                <h5>In class</h5>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+            <Link >
+                
+                <div className='sidebarChat'>
+                    <Avatar   />
+                    <div className='sidebarChat__info'>
+                    <h2>name</h2>
+                    <p>Hello</p>
+
+                    </div>
+                    
+                </div>
+            </Link>
+
+
+            </div>
 
             </div>
         </div>
